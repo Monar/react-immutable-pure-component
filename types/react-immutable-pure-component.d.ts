53c59cd8a3d16f5b14523ffee0d8da80a192b8b5
Piotr Monarski <pmn@touk.pl>
2018-08-03 10:57:19 +0200

    Fix/Upgrade typescript typing, loosen peer dependencies
    * change typescript typing
    * loosen peer dependencies
    * add optional @type/react dependency

--- types/react-immutable-pure-component.d.ts
+++ types/react-immutable-pure-component.d.ts
@@ -1,8 +1,8 @@
-import * as React from 'react';
-declare module 'react-immutable-pure-component' {
-  export class ImmutablePureComponent<P,S> extends React.Component<P,S> {
+export as namespace ImmutablePureComponent;
+
+import { Component } from 'react';
+
+export class ImmutablePureComponent<P = {}, S = {}, SS = any> extends Component<P, S, SS> {
       updateOnProps: Array<keyof P>;
       updateOnStates: Array<keyof S>;
-      shouldComponentUpdate(nextProps: P, nextState?: S): boolean;
-  }
 }
