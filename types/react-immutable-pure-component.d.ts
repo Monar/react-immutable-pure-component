70dbdd77cd3001162e8adc17f3f5c5e34f34d3cc
Piotr Monarski <dev@monarski.pl>
2019-11-06 23:44:41 +0100

    Update typescript types, update (dev-)dependencies, drop optional dependency

--- types/react-immutable-pure-component.d.ts
+++ types/react-immutable-pure-component.d.ts
@@ -1,10 +1,10 @@
 export as namespace ImmutablePureComponent;
 
-import { Component, NamedExoticComponent } from 'react';
+import { Component, ComponentType, NamedExoticComponent, MemoExoticComponent, FC } from 'react';
 
 type UpdateOn<T> = Array<keyof T | any[]>
 
-export function immutableMemo<P extends object>( Component: SFC<P>, updateOnProps?: UpdateOn<P>): NamedExoticComponent<P>;
+export function immutableMemo<P extends object>( Component: FC<P>, updateOnProps?: UpdateOn<P>): NamedExoticComponent<P>;
 export function immutableMemo<T extends ComponentType<any>>( Component: T, updateOnProps?: UpdateOn<T>): MemoExoticComponent<T>;
 
 export class ImmutablePureComponent<P = {}, S = {}, SS = any> extends Component<P, S, SS> {
